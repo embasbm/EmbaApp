1a775cfada813ca40b3eb808f7c440ffac96a62c
Emba Moussa <embasbm@gmail.com>
2018-05-27 15:19:33 +0200

    fix: first name and last name from faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ User.destroy_all
 
 50.times do |index|
   User.create(
-    first_name: Faker::Name.name,
-    last_name: Faker::Name.name,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
   )
 end
