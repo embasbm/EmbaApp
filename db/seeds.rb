fe2fde03002ee9a28c9efe5161d03ca5eeef2149
Emba Moussa <embasbm@gmail.com>
2018-05-27 15:30:10 +0200

    feat: basic unit test for User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@ User.destroy_all
   User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
+    email: Faker::Internet.email
   )
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) { @user = FactoryBot.create(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:email) }
+
+  it '#first_name should responde to type String' do
+    expect(@user.first_name).to be_a String
+  end
+
+  it '#last_name should responde to type String' do
+    expect(@user.last_name).to be_a String
+  end
+
+  it '#email should responde to type String' do
+    expect(@user.email).to be_a String
+  end
 end
