3fa587af5da11c6e46e104dcb6e287438ea3e17d
Emba Moussa <emba.moussa@eventinc.de>
2018-05-30 18:01:47 +0200

    fix: fix view test will paginate

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,24 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
   before(:each) do
-    assign(:users, [
-      User.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
-        :email => "Email"
-      ),
-      User.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
-        :email => "Email"
-      )
-    ])
+    @users = WillPaginate::Collection.new(4,10,0)
+
+    3.times do
+      @users << User.create!(first_name: "First Name", last_name: "Last Name", email: "Email")
+    end
+    assign(:users, @users)
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", text: "First Name".to_s, count: 3
+    assert_select "tr>td", text: "Last Name".to_s, count: 3
+    assert_select "tr>td", text: "Email".to_s, count: 3
   end
 end
